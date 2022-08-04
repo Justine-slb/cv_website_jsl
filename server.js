93c353c6c4a537fe05bf06a1d91aebad5bf6ba88
JSEGUINLEBOULANGER <justine.seguinleboulanger@ynov.com>
2022-08-04 17:34:44 +0200

    web page ok miss link portfolio

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const hbs = require('hbs');
+const path = require('path');
+
+// Server Creation //
+const app = express()
+app.use(express.static('static'));
+const port = 8080;
+// Template & Component setup //
+
+app.set('views', 'static');
+app.set('view engine', 'hbs');
+
+
+hbs.registerPartials(path.join(__dirname, 'static/partials'));
+
+let route = require("./backend/app");
+app.use("/",route);
+
+/*for load all other page in 404*/
+app.listen(port, () => {
+    console.log(`[Server] App listening on port ${port}`)
+})
